2a4094a59b42818b141881b5a554684a025e4d5d
suhailphp <suhailphp@gmail.com>
2021-02-01 10:14:09 +0400

    Image document reading success with loading gif

--- src/controllers/ocr.js
+++ src/controllers/ocr.js
@@ -5,7 +5,14 @@ const debug = require('debug')(appConfig.APP_NAME + ":CONTROLLER-VehicleType.js"
 const i18n = require("i18n");
 const {gatekeeper} = require('../middlewares/index');
 
-const fs = require('fs')
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+const {createWorker} = require('tesseract.js');
+const worker =  createWorker();
+
+
 
 
 const Utils = require('../utilities');
@@ -15,7 +22,7 @@ const base_url = '/ocr/';
 
 
 
-let mainPage = 'OCR';
+let currentPage = 'OCR';
 
 module.exports = (Router, Models) => {
     // options are csrfProtection, parseForm
@@ -27,17 +34,53 @@ module.exports = (Router, Models) => {
             gatekeeper.authorization(['ADMIN']),
             async (req, res, next) => {
 
-                res.send('hai')
+                let data = {
+                    title: req.__('OCR'),
+                    breadcrumb: Utils.breadcrumbs.init(__filename, moduleName).add('view OCR'),
+                    currentPage,
+                    CurrentDate: new Date()
+                }
+
+                res.render('home',data)
+
+
 
             })
 
+    Router
+        .get('/process',
+            gatekeeper.authorization(['ADMIN']),
+            async (req, res, next) => {
+
+                let data = {
+                    title: req.__('OCR'),
+                    breadcrumb: Utils.breadcrumbs.init(__filename, moduleName).add('view OCR'),
+                    currentPage,
+                    CurrentDate: new Date()
+                }
+
+                try{
+                    let fileData = await readFile('./files/img.png')
+
+                    await worker.load();
+                    await worker.loadLanguage('eng+ara');
+                    await worker.initialize('eng+ara');
+                    // await worker.setParameters({
+                    //     tessjs_create_pdf: '1',
+                    // });
+                    const {data: {text}} = await worker.recognize(fileData);
+                    await worker.terminate();
+                    data.text = text
+                    res.render('form', data)
+                }
+                catch (error){
+                    console.log('some error ->'+error)
+                    res.send(error.message)
+                }
 
 
 
-
-
-
-
+            })
 
 
     return Router
